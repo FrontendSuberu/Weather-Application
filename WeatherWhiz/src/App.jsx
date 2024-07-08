d1eaf1dafb1724d4b015ad83d3a86f757ff862e1
Front_End_SuberuJoe <156342887+FrontendSuberu@users.noreply.github.com>
2024-07-08 16:09:17 +0100

    changes of route holder

--- WeatherWhiz/src/App.jsx
+++ WeatherWhiz/src/App.jsx
@@ -1,10 +1,29 @@
 import './App.css'
+import { BrowserRouter } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
+import Home from '../src/Pages/Home/Home'
+import Weather from '../src/Pages/Weather/Weather'
 
 function App() {
 
   return (
     <>
-    <p>Hello People</p>
+   {/* browserrouter */}
+   <BrowserRouter>
+
+{/* desendant of browser router */}
+<Routes>
+{/* routes */}
+    <Route path='/' element={<Home />} />
+    <Route path='/weatherWhiz-Weather' element={<Weather />} />
+{/* routes */}
+
+</Routes>
+{/* desendant of browser router */}
+
+</BrowserRouter>
+{/* browserrouter */}
     </>
   )
 }
